4a6f5ded777e503af18dddea269f6c6ba142df88
Nick McKendry <nickmckendry@gmail.com>
2017-05-18 17:02:24 -0600

    finished

---
+++ app.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//   collapseForm()
+//   hideForm()
+// })
+//
+// function collapseForm(event){
+//   $('.collapser').click(function() {
+//
+//     $('.collapse').collapse()
+//   })
+// }
+//
+// function hideForm(){
+//   $('.close-collapser').click(function() {
+//     $('.collapse').collapse()
+//   })
+// }
