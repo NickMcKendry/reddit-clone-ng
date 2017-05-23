abc7d4454fa4f7eb1e630d6d0cdad381ebf08ddf
Nick McKendry <nickmckendry@gmail.com>
2017-05-23 10:11:09 -0600

    first commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,23 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const router = express.Router()
+const bodyParse = require('body-parser')
 
-const app = express()
+const port = process.env.PORT || 6500
 
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/../', 'node_modules')))
+
+router.get('/*', (req, res, next) => {
+  const options = {
+    root: __dirname + '/../redditApp'
+  }
+
+  res.sendFile('index.html', options, (err) => {
+    if(err) return next(err)
+
+    console.log('Index file sent to browser');
+  })
+})
+
+module.exports = router
